ffe1625743738b06a0cd911f20c7bce9583cc0c1
Tomas Kubin <kubin.tom@email.cz>
2021-11-18 10:25:35 +0100

    add array js example

---
+++ javascript/arrays/script.js
@@ -0,0 +1,21 @@
+
+
+const fruits = [
+    {
+        name: 'Apple',
+        color: 'red',
+        img: 'https://grocery11.com/wp-content/uploads/2021/04/apple-fruit-500x500-1.jpg'
+    },
+    {
+        name: 'Banana',
+        color: 'yellow',
+        img: 'https://5.imimg.com/data5/VY/QT/MY-51857835/banana-fruit-500x500.jpg'
+    },
+    {
+        name: 'Plum',
+        color: 'Purple',
+        img: 'https://static.libertyprim.com/files/familles/prune-large.jpg?1569271837'
+    },
+];
+
+
