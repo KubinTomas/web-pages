c798b808568cae0e09fc89538b31a4e7e69f455b
Tomas Kubin <kubin.tom@email.cz>
2021-12-01 09:29:37 +0100

    add function which open last active panel

--- small-pages/00-expanding-cards/script.js
+++ small-pages/00-expanding-cards/script.js
@@ -1,8 +1,13 @@
 const panels = document.querySelectorAll('.panel');
 
-panels.forEach(panel => {
+panels.forEach((panel, index) => {
+
+    openLastActivePanel(panel, index);
+
     panel.addEventListener('click', () => {
         removeActiveClasses();
+
+        localStorage.setItem('panelIndex', index);
 
         panel.classList.add('active');
     });
@@ -12,4 +17,16 @@ function removeActiveClasses() {
     panels.forEach(panel => {
         panel.classList.remove('active');
     });
+}
+
+function openLastActivePanel(panel, index) {
+    const panelIndex = localStorage.getItem('panelIndex');
+
+    if (!panelIndex) {
+        panelIndex = 0;
+    }
+
+    if (Number(panelIndex) === index) {
+        panel.classList.add('active');
+    }
 }
