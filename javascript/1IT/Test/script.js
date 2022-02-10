525b69434c149735dcb2ece6eca2b0b1c6aad5dc
Tomas Kubin <kubin.tom@email.cz>
2022-02-10 14:07:30 +0100

    add start tree

--- javascript/1IT/Test/script.js
+++ javascript/1IT/Test/script.js
@@ -10,12 +10,15 @@ function showMessage(message) {
 const array = [6, "x", 2, 3, "5", 2, "3"];
 const array2 = [3, "5", 2, "3"];
 getOnlyNumbers(array);
+getOnlyNumbers(array2);
 
 function getOnlyNumbers(arrayOfEverything) {
+    // return arrayOfEverything.filter(c =>  typeof c === 'number');
     let tmpArray = [];
+
     for (let i = 0; i < arrayOfEverything.length; i++) {
         const element = arrayOfEverything[i];
-
+        // 
         if (typeof element === 'number') {
             tmpArray.push(element);
         }
@@ -27,7 +30,8 @@ function getOnlyNumbers(arrayOfEverything) {
 const res = isDivider(10, 2);
 console.log(res);
 function isDivider(x, y) {
-    return x % y == 0;
+    // x > 2 -> true/false -> vstup do ifu
+    return x % y === 0;
 }
 
 const num = createNumbersUntil(10);
@@ -35,9 +39,31 @@ console.log(num);
 function createNumbersUntil(n) {
     let numbers = [];
 
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i <= n; i++) {
         numbers.push(i);
     }
 
     return numbers;
+}
+
+//*
+//**
+//***
+
+console.log('tree');
+console.log(buildTree(3));
+function buildTree(floorCount) {
+    let tree = '';
+    let stars = '*';
+
+    for (let i = 1; i <= floorCount; i++) {
+        // 1,2,3
+        tree = tree + stars;
+
+        tree += '\n';
+
+        stars = stars + '*';
+    }
+
+    return tree;
 }
