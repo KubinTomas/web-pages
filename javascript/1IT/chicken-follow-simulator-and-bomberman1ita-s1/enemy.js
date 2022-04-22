0aed867c2e95d1fff39db8cd7deb7fbb40e480d7
Tomas Kubin <kubin.tom@email.cz>
2022-04-22 09:41:55 +0200

    super epicka slepici gameska

---
+++ javascript/1IT/chicken-follow-simulator-and-bomberman1ita-s1/enemy.js
@@ -0,0 +1,46 @@
+const enemies = [];
+
+function createEnemy() {
+    const enemyElement = document.createElement('div');
+
+    enemyElement.style.width = '68.9px';
+    enemyElement.style.height = '68.9px';
+    enemyElement.style.backgroundColor = 'darkred';
+    enemyElement.style.borderRadius = '7.5px';
+
+    enemyElement.style.position = 'absolute';
+    enemyElement.style.top = '100px';
+    enemyElement.style.left = '100px';
+
+    document.body.appendChild(enemyElement);
+
+    return enemyElement;
+}
+
+setInterval(() => {
+    enemies.push(createEnemy());
+}, 1000);
+
+setInterval(() => {
+    moveEnemiesToPlayer();
+}, 10);
+
+function moveEnemiesToPlayer() {
+    enemies.forEach(enemy => {
+        const enemyTop = Number(enemy.style.top.slice(0, enemy.style.top.length - 2));
+        const playerTop = Number(player.style.top.slice(0, player.style.top.length - 2));
+
+        const enemyleft = Number(enemy.style.left.slice(0, enemy.style.left.length - 2));
+        const playerleft = Number(player.style.left.slice(0, player.style.left.length - 2));
+        moveEnemyVertical(enemy, enemyTop > playerTop ? -1 : 1);
+        moveEnemyHorizontal(enemy, enemyleft > playerleft ? -1 : 1);
+    });
+}
+
+function moveEnemyVertical(enemy, direction) {
+    enemy.style.top = Number(enemy.style.top.slice(0, enemy.style.top.length - 2)) + direction * 1 + 'px';
+}
+
+function moveEnemyHorizontal(enemy, direction) {
+    enemy.style.left = Number(enemy.style.left.slice(0, enemy.style.left.length - 2)) + direction * 1 + 'px';
+}
