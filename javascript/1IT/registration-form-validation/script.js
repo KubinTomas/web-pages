cdf4132db32c8ed74ddb422668fcf6f87f91338f
Tomas Kubin <kubin.tom@email.cz>
2022-05-12 12:31:38 +0200

    add form name validation

--- javascript/1IT/registration-form-validation/script.js
+++ javascript/1IT/registration-form-validation/script.js
@@ -1,4 +1,40 @@
 function validateRegistrationForm() {
 
-    return false;
+    const nameElement = document.forms['registrationForm']['name'];
+
+    const invalidName = validateName(nameElement);
+
+    if (invalidName) {
+        return false;
+    }
+}
+
+function validateName(nameElement) {
+    let invalid = false;
+
+    const parent = nameElement.parentElement;
+    const errorTextElement = parent.querySelector('.error-text');
+
+    parent.classList.remove('error');
+
+    // kontrola validity
+    const value = nameElement.value;
+
+
+    if (value.length <= 2) {
+        errorTextElement.innerHTML = "Minimum 3 chars for name!";
+        invalid = true;
+    }
+
+
+    if (value.length === 0) {
+        errorTextElement.innerHTML = "Name is required!";
+        invalid = true;
+    }
+
+    if (invalid) {
+        parent.classList.add('error');
+    }
+
+    return invalid;
 }
