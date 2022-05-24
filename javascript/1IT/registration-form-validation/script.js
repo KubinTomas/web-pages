4598cf86c907f0a7cea136b71d2ecb5f8356042f
Tomas Kubin <kubin.tom@email.cz>
2022-05-24 13:10:27 +0200

    add email validation

--- javascript/1IT/registration-form-validation/script.js
+++ javascript/1IT/registration-form-validation/script.js
@@ -1,14 +1,51 @@
 function validateRegistrationForm() {
     let formValid = true;
     const nameElement = document.forms['registrationForm']['name'];
+    const emailElement = document.forms['registrationForm']['email'];
     console.log(nameElement);
 
     if (!isNameInputValid(nameElement)) {
         formValid = false;
     }
 
+    if (!isEmailInputValid(emailElement)) {
+        formValid = false;
+    }
+
     return formValid;
 }
+
+
+
+function isEmailInputValid(emailElement) {
+    let valid = true;
+
+    const parentElement = emailElement.parentElement;
+    const errorTextElement = parentElement.querySelector('.error-text');
+
+    const value = emailElement.value;
+    parentElement.classList.remove('error');
+
+    if (!validateEmail(value)) {
+        valid = false;
+        errorTextElement.innerHTML = 'Invalid email';
+    }
+
+    if (!valid) {
+        parentElement.classList.add('error');
+    }
+
+    return valid;
+}
+
+function validateEmail(email) {
+
+    return String(email)
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+};
 
 function isNameInputValid(nameElement) {
     let valid = true;
